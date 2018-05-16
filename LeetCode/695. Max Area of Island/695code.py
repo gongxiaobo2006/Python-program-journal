6f8beabdc9fee10742f649858d6c682781dc9f28
Peihan <lphs1234567@gmail.com>
2018-05-16 10:22:02 +0800

    Add files via upload

---
+++ LeetCode/695. Max Area of Island/695code.py
@@ -0,0 +1,40 @@
+#https://leetcode.com/problems/max-area-of-island/description/
+#LeetCode 695. Max Area of Island
+
+#author:peihan
+#date: 05/15/2018
+
+
+
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,1,1,0,1,0,0,0,0,0,0,0,0],
+[0,1,0,0,1,1,0,0,1,0,1,0,0],
+[0,1,0,0,1,1,0,0,1,1,1,0,0],
+[0,0,0,0,0,0,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+
+rows, cols = len(grid), len(grid[0])
+
+
+def dfs(i, j):
+	if 0 <= i < rows and 0 <= j < cols and grid[i][j]: # grid[i][j]表示当前位置上有值 
+		grid[i][j] = 0
+		return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
+	return 0
+
+result = 0
+for x in range(rows):
+	for y in range(cols):
+		if grid[x][y]: # grid[x][y]表示当前位置上有值 
+			result = max(result, dfs(x, y))
+
+print (result)
+
+
+
+
+
